2b22939310564b2a5eda009f815ae66bda5d3de2
ManuelPanduro18 <edson.panduro@tecsup.edu.pe>
2019-05-10 16:22:08 -0500

    commit

---
+++ src/main/java/com/tecsup/lab10/Operadores_math/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab10.Operadores_math;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
